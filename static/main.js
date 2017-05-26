54e55376953ccec8f3731770d03e025060bfbdc3
Arnelle Balane <arnellebalane@gmail.com>
2017-05-27 03:31:46 +0800

    translate beta rotation to css rotation

--- static/main.js
+++ static/main.js
@@ -2,12 +2,27 @@ const socket = io('/');
 socket.on('deviceorientation', handleOrientation);
 
 const element = document.querySelector('.device');
-const offsets = { beta: 90 };
 
-function handleOrientation({ alpha, beta, gamma }) {
+function handleOrientation(angles) {
+    const rotateX = computeRotateX(angles);
+    const rotateY = computeRotateY(angles);
     const transforms = [
-        `rotateX(${-beta + offsets.beta}deg)`,
-        `rotateY(${gamma}deg)`
+        `rotateX(${rotateX}deg)`,
+        `rotateY(${rotateY}deg)`
     ];
     element.style.transform = transforms.join(' ');
+}
+
+function computeRotateX({ beta }) {
+    // beta(x) = [-180, 180]
+    // [1] Convert beta orientation to [0, 360] range
+    // [2] Translate angle to be compatible with CSS Transforms rotations
+    beta = beta > 0 ? beta : 360 + beta; // [1]
+    beta = (360 - (beta - 90)) % 360;    // [2]
+    return beta;
+}
+
+function computeRotateY({ beta, gamma }) {
+    // beta(x) = [-180, 180], gamma(y) = [-90, 90]
+    return gamma;
 }
